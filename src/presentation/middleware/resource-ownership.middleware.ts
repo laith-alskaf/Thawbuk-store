6596c2ef9b63c63492f6545f5ac301fb0afcfe2e
laith-alskaf <liethmhmad231@gmail.com>
2025-09-09 22:38:30 +0300

    Fix problem in delete product route

--- src/presentation/middleware/resource-ownership.middleware.ts
+++ src/presentation/middleware/resource-ownership.middleware.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import { Messages, StatusCodes, UserRoles } from '../config/constant';
 import { Model } from 'mongoose';
 import { ForbiddenError } from '../../application/errors/application-error';
+import { CategoryModel } from '../../infrastructure/database/mongodb/models/category.model';
+import { ProductModel } from '../../infrastructure/database/mongodb/models/product.model';
 
 export interface IOwnership {
     createdBy: string;
@@ -18,7 +20,15 @@ export const checkResourceOwnership = <T extends IOwnership>(
                 throw new Error(Messages.AUTH.AUTHENTICATION_REQUIRED);
             }
             const resourceId = req.body[idKey] || req.params[idKey];
-            const resource = await model.findById(resourceId);
+            var resource;
+            if (idKey == 'productId') {
+                resource = await ProductModel.findById(resourceId);
+            }
+            if (idKey == 'categoryId') {
+                resource = await CategoryModel.findById(resourceId);
+            }
+
+
             if (!resource) {
                 throw new Error(Messages.GENERAL.INVALID_PARAMETERS);
             }
